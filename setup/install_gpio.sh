1b0fb96cdaa0c36f2c120e9ddc131a940b225456
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2023-01-11 17:38:27 +0900

    Update install_gpio.sh

--- setup/install_gpio.sh
+++ setup/install_gpio.sh
@@ -2,13 +2,13 @@
 # git clone from source
 git clone https://github.com/NVIDIA/jetson-gpio.git
 cd jetson-gpio
-sudo python3 setup.py install
+python3 setup.py install
 
 # create group and add user
-sudo groupadd -f -r gpio
-sudo usermod -a -G gpio <ユーザー名>
+groupadd -f -r gpio
+usermod -a -G gpio <ユーザー名>
 
 # add udev rule
-sudo cp lib/python/Jetson/GPIO/99-gpio.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules && sudo udevadm trigger
-sudo reboot
+cp lib/python/Jetson/GPIO/99-gpio.rules /etc/udev/rules.d/
+udevadm control --reload-rules && sudo udevadm trigger
+reboot
