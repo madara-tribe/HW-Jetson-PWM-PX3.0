f09693f8934fd238410d5f81919b76997c2b1c26
madara-tribe <tsukuba.univer@gmail.com>
2024-06-13 09:41:04 +0900

    px3.1

--- calibrater.py
+++ calibrater.py
@@ -1,8 +1,7 @@
 import argparse
 import os
 from tkinter import Tk
-from Adafruit_PCA9685_.pan_tilt import App
-#from Adafruit_PCA9685_.dual_servo import calibrate
+
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -13,21 +12,22 @@ def get_parser():
     parser.add_argument('--y_max', type=int, default=180, help='y angle max')
     parser.add_argument('--y_min', type=int, default=0, help='y angle min')
     parser.add_argument('--y_defalut', type=int, default=169, help='y default angle')
-    parser.add_argument('--x_defalut', type=int, default=66, help='x default angle')
-    parser.add_argument('--set_time', type=int, default=1, help='set timer')
+    parser.add_argument('--x_defalut', type=int, default=59, help='x default angle')
     opt = parser.parse_args()
     return opt
 
 
 def main(opt):
     if opt.gui:
+        from Adafruit_PCA9685_.tkinter_pan_tilt import App
         root = Tk()
         root.wm_title('Servo Control')
         app = App(root)
         root.geometry("220x120+0+0")
         root.mainloop()
     elif opt.command:
-        os.system('python3 Adafruit_PCA9685_/dual_servo.py')
+        from Adafruit_PCA9685_.dualservo_calicurate import calibrate
+        calibrate(opt)
 
 
 if __name__=='__main__':
